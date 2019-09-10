7e1c4b95c011f794f26e7d1639bd6b090c27f74f
coukii <51959984+coukii@users.noreply.github.com>
2019-09-10 15:21:11 +0100

    tiles are now larger, added selection mechanic

--- main.py
+++ main.py
@@ -21,31 +21,54 @@ clock = pygame.time.Clock()
 class Tile(pygame.sprite.Sprite):
     def __init__(self, letter, score, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50,50))
+        self.image = pygame.Surface((70,70))
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
         self.rect.left = x
         self.rect.top = y
-        self.letterfont = pygame.font.SysFont('Times New Roman', 30)
-        self.scorefont = pygame.font.SysFont('Times New Roman', 12)
+        self.letterfont = pygame.font.SysFont('Times New Roman', 36)
+        self.scorefont = pygame.font.SysFont('Times New Roman', 16)
         self.letter = letter
         self.score = score
         self.letterrender = self.letterfont.render(self.letter, True, BLACK)
         self.scorerender = self.scorefont.render(self.score, True, BLACK)
-        self.fontsize = self.letterfont.size(letter)
         #pygame.display.update
 
     def update(self):
-        screen.blit(self.letterrender, (self.rect.centerx - (self.fontsize[0] / 2.), self.rect.centery - (self.fontsize[1] / 2.)))
-        screen.blit(self.scorerender, (self.rect.right - 10, self.rect.bottom - 14))
+        screen.blit(self.letterrender, (self.rect.centerx - (self.letterfont.size(self.letter)[0] / 2.), self.rect.centery - (self.letterfont.size(self.letter)[1] / 2.)))
+        screen.blit(self.scorerender, (self.rect.right - (self.scorefont.size(self.score)[0]) - 2, self.rect.bottom - (self.scorefont.size(self.score)[1])))
+
+class Selection(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((70, 70))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.left = x
+        self.rect.top = y
 
 all_sprites = pygame.sprite.Group()
+
+board=[[0,0,0,0],
+       [0,0,0,0],
+       [0,0,0,0],
+       [0,0,0,0]]
+
 for column in range(0,4):
     for row in range(0,4):
         tileInfo = random.choice(list(LETTERSCORES.items()))
-        tile = Tile(tileInfo[0], str(tileInfo[1]), WIDTH / 2 + column * 50 + column * 10 - 115, HEIGHT / 2 + row * 50 + row * 10 - 115)
+        tile = Tile(tileInfo[0], str(tileInfo[1]), (WIDTH / 2) + (column * 70) + (column * 10) - 155, (HEIGHT * 0.75) + (row * 70) + (row * 10) - 155)
         all_sprites.add(tile)
+        print("adding row", row, "column", column)
+        board[row][column] = tile
 
+selectedTile = board[0][0]
+
+selection = Selection(selectedTile.rect.left, selectedTile.rect.top)
+all_sprites.add(selection)
+
+# ADD GREEN TILES TO EVERY TILE OBJECT (BLACK AS DEFAULT)
+# ENABLE WHEN SELECTED
 
 # Game loop
 running = True
